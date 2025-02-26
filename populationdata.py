ceb5c23a412e19a362bf5d8cd8de8a5e9b00a96a
sarailainez <sarailinarez2006@gmail.com>
2025-02-25 23:12:04 -0800

    commit 1

---
+++ populationdata.py
@@ -0,0 +1,8 @@
+population_data = \
+[
+    {"Geographic_Area": "New York city, New York", "Pop_Est_Apr_2020": 8419600, "Pop_Est_Jul_2020": 8438271, "Pop_Est_Jul_2021": 8399000},
+    {"Geographic_Area": "Los Angeles city, California", "Pop_Est_Apr_2020": 3980400, "Pop_Est_Jul_2020": 3990456, "Pop_Est_Jul_2021": 3968000},
+    {"Geographic_Area": "Chicago city, Illinois", "Pop_Est_Apr_2020": 2716000, "Pop_Est_Jul_2020": 2725000, "Pop_Est_Jul_2021": 2700000},
+    {"Geographic_Area": "Houston city, Texas", "Pop_Est_Apr_2020": 2328000, "Pop_Est_Jul_2020": 2335000, "Pop_Est_Jul_2021": 2342000},
+    {"Geographic_Area": "Phoenix city, Arizona", "Pop_Est_Apr_2020": 1690000, "Pop_Est_Jul_2020": 1702000, "Pop_Est_Jul_2021": 1718000},
+]
